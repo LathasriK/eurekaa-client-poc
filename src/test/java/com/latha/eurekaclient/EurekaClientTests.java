a341db891789412f558c5866d34e184755d21991
LathasriK <82448459+LathasriK@users.noreply.github.com>
2021-10-14 14:45:26 +0530

    Add files via upload

---
+++ src/test/java/com/latha/eurekaclient/EurekaClientTests.java
@@ -0,0 +1,13 @@
+package com.latha.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
